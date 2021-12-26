b31deabcd895112406e294d49a193c1eaacc2931
MKTunca <91753517+MKTunca@users.noreply.github.com>
2021-12-27 00:21:26 +0300

    Add files via upload

---
+++ WindowsFormsApp13/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp13
+{
+    class fiyat
+
+    {
+        public double carpma(double litre)
+        
+        {
+            double sonuc = 0;
+            
+            
+            {
+                if ((litre > 0) && (litre <= 2))
+                {
+                    sonuc = litre * 5.5;
+                }
+
+                else if ((litre > 2) && (litre <= 5))
+
+                {
+                    sonuc = litre * 5;
+                }
+                else if ((litre > 5) && (litre <= 7))
+                {
+                    sonuc = litre * 4.5;
+                }
+                else if ((litre > 7) && (litre <= 10))
+                {
+                    sonuc = litre * 4;
+                }
+                else if (litre > 10)
+                {
+                    sonuc = litre * 3.5;
+                }
+                else
+                {
+
+                }
+                return sonuc;
+
+
+
+            }
+
+
+        }
+    }
+}
---
+++ WindowsFormsApp13/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        fiyat sonuc;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double l = Convert.ToDouble(lt.Text);
+            
+           {
+
+                if (lt.Text == "")
+                {
+                    MessageBox.Show("sayı giriniz");
+                }
+                else
+                {
+                    label1.Text =Convert.ToString(sonuc);
+                }
+                
+           }    
+        }
+        
+
+
+
+
+    }
+}
